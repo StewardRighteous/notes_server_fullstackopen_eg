902f1886d272227b0b5e1ec401d2f6d8732fa8f0
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2026-06-08 10:44:19 +0530

    note_server_fullstackopen_eg

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.get("/api/notes/:id", (req, resp) => {
+  const id = req.params.id;
+  const note = notes.find((n) => n.id === id);
+  note ? resp.json(note) : resp.status(404).end();
+});
+
+app.delete("/api/notes/:id", (req, resp) => {
+  const id = req.params.id;
+  notes = notes.filter((n) => n.id !== id);
+  resp.status(204).end();
+});
+
+app.post("/api/notes", (req, resp) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return resp.status(400).json({ error: "content missing" });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  resp.json(note);
+});
+
+app.get("/", (req, resp) => resp.send("<h1>Hello World </h1>"));
+
+app.get("/api/notes", (req, resp) => resp.json(notes));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
